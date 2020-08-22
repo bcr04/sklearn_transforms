0d732dfef94fd7cf59f1b03218532cfcdca9de0e
Unknown <unknown@example.com>
2020-08-22 19:37:08 -0300

    teste

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
+      url='https://github.com/bcr04/sklearn_transforms/',
+      author='bcr04',
+      author_email='',
       license='BSD',
       packages=[
-            'my_custom_sklearn_transforms'
+            'my_custom_sklearn_transforms_maratona_IBM_desafio_02'
       ],
       zip_safe=False
 )
